5bae3f34d0314f7571cd9e452076e79eea33eac1
Matthew Condit <mcondit@colgate.edu>
2016-10-26 17:59:15 -0400

    Back at davids site

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    var error = req.query.error;
-    console.log(error);
-    res.render('index', { title: 'Condit Construction' });
+router.get('/', function (req, res, next) {
+  var error = req.query.error;
+  console.log(error);
+  res.render('index', {
+    title: 'Condit Construction'
+  });
 });
-
+/* GET trim page */
+router.get('/trim', function (req, res, next) {
+  res.render('trim', {
+    title: 'Trim'
+  })
+});
+/* GET plowing page */
+router.get('/plowing', function (req, res, next) {
+  res.render('plowing', {
+    title: 'Snow Plowing'
+  });
+});
 module.exports = router;
